d52e5f1864e2d4c38924c859a96d48dda2dad0b5
Harshith-01 <2004harshithkumar2004@gmail.com>
2025-10-06 11:52:58 +0530

    Update main.py
    cors changes

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # --- CORS Middleware ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # Your frontend origin
+    allow_origins=["http://localhost:3000","https://medical-data-generator-liart.vercel.app"], # Your frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
